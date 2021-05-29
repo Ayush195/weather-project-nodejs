55225c428231f21819973dd1bc8ccde1e106c3fa
Ayush Shukla <helloayush195@gmail.com>
2021-05-29 09:58:31 +0530

    Data1

---
+++ public/js/main.js
@@ -0,0 +1,54 @@
+const cityName  = document.getElementById('cityName');
+const submitBtn = document.getElementById('submitBtn');
+const city_name = document.getElementById('city_name');
+const temp_real_val = document.getElementById('temp_real_val');
+const temp_status = document.getElementById('temp_status');
+const datahide = document.querySelector('.middle_layer');
+
+const getInfo = async (event) =>{
+    event.preventDefault();
+    
+    let cityValue = cityName.value;
+    if(cityValue === ""){
+        city_name.innerText = `Plz Write The Name Before Search`;
+        datahide.classList.add('data_hide');
+    }else{
+        try{
+            let url = `http://api.openweathermap.org/data/2.5/weather?q=${cityValue}&units=metric&appid=e1183f82dcb8e00bd605c5cb88a6c984`;
+            const response = await fetch(url);
+            const data = await response.json();
+            const arrData = [data];
+            console.log(data);
+            city_name.innerText = `${arrData[0].name} , ${arrData[0].sys.country}`;
+            temp_real_val.innerText = arrData[0].main.temp;
+        
+            const tempMood = arrData[0].weather[0].main;
+
+            //condition to check sunny or cloudy
+            
+            if(tempMood == "Clear")
+            {
+                temp_status.innerHTML=
+                "<i class='fas fa-sun' style='color: #eccc68;'></i>";
+            }else if(tempMood == "Clouds"){
+                temp_status.innerHTML=
+                "<i class='fas fa-cloud' style='color: #f1f2f6;'></i>";
+            }else if(tempMood == "Rain"){
+                temp_status.innerHTML=
+                "<i class='fas fa-cloud-rain' style='color: #a4b0be;'></i>";
+            }else{
+                temp_status.innerHTML=
+                "<i class='fas fa-sun' style='color: #eccc68;'></i>";
+            }
+            datahide.classList.remove('data_hide');
+        }
+        catch{
+            city_name.innerText = `Plz Enter The City Name Properly`;
+            datahide.classList.add('data_hide');
+        }
+        
+    }
+    
+}
+
+submitBtn.addEventListener('click',getInfo);
